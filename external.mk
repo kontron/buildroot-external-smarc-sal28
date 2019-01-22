66bb100fe88eebb80b18645a2f7e356985ef668a
Michael Walle <michael.walle@kontron.com>
2019-01-22 19:05:54 +0100

    ls1028a-dp-firmware: new package
    Signed-off-by: Michael Walle <michael.walle@kontron.com>

--- external.mk
+++ external.mk
@@ -1,1 +1,2 @@
-include $(sort $(wildcard $(BR2_EXTERNAL_SMARC_SAL28)/package/*/*.mk))
+include $(BR2_EXTERNAL_SMARC_SAL28_PATH)/package/freescale-helper.mk
+include $(sort $(wildcard $(BR2_EXTERNAL_SMARC_SAL28_PATH)/package/*/*.mk))
