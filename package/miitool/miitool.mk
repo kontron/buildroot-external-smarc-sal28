a73df559ca67ab8043d3c59e7f3dad14c5e9bd5f
Michael Walle <michael.walle@kontron.com>
2019-05-14 13:44:25 +0200

    miitool: new package
    Signed-off-by: Michael Walle <michael.walle@kontron.com>

---
+++ package/miitool/miitool.mk
@@ -0,0 +1,14 @@
+MIITOOL_VERSION = 1.1
+MIITOOL_SITE = $(call github,kontron,miitool,$(MIITOOL_VERSION))
+MIITOOL_LICENSE = BSD-2c
+MIITOOL_LICENSE_FILES = COPYING
+
+define MIITOOL_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
+endef
+
+define MIITOOL_INSTALL_TARGET_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) DESTDIR=$(TARGET_DIR) install
+endef
+
+$(eval $(generic-package))
