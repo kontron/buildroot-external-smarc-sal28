29708af48ef143d8a5ac172645e5b2ecd92f1ac3
Michael Walle <michael.walle@kontron.com>
2019-05-29 12:24:57 +0200

    sl28: make more space for fdt overlays
    Signed-off-by: Michael Walle <michael.walle@kontron.com>

--- board/kontron/smarc-sal28/boot.cmd
+++ board/kontron/smarc-sal28/boot.cmd
@@ -4,9 +4,9 @@ load ${devtype} ${devnum} ${kernel_addr_r} Image
 load ${devtype} ${devnum} ${fdt_addr_r} sl28.dtb
 
 echo "Preparing for overlays"
-setexpr fdt_overlay_addr_r ${fdt_addr_r} + F000
+setexpr fdt_overlay_addr_r ${fdt_addr_r} + 10000
 fdt addr ${fdt_addr_r}
-fdt resize
+fdt resize 4000
 
 if load ${devtype} ${devnum} ${fdt_overlay_addr_r} overlays/sl28-variant${variant}.dtbo; then
 	echo "Overlaying sl28-variant${variant}.dtbo"
