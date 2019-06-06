cce10a6205dfcee7807a2c98dc21d0f6ceb84bb1
Michael Walle <michael.walle@kontron.com>
2019-06-06 13:49:49 +0200

    tsntool: new package
    Signed-off-by: Michael Walle <michael.walle@kontron.com>

---
+++ package/tsntool/tsntool.mk
@@ -0,0 +1,29 @@
+################################################################################
+#
+# tsntool
+#
+################################################################################
+
+TSNTOOL_VERSION = ls1028a-early-access-bsp0.3
+TSNTOOL_SITE = https://source.codeaurora.org/external/qoriq/qoriq-components/tsntool
+TSNTOOL_SITE_METHOD = git
+TSNTOOL_LICENSE = MIT or GPL-2.0+
+TSNTOOL_LICENSE_FILES = LICENSE
+TSNTOOL_INSTALL_STAGING = YES
+TSNTOOL_DEPENDENCIES = libnl readline ncurses cjson
+
+define TSNTOOL_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) build
+endef
+
+define TSNTOOL_INSTALL_STAGING_CMDS
+	$(INSTALL) -D -m 0644 $(@D)/include/tsn/genl_tsn.h $(STAGING_DIR)/usr/include/tsn/genl_tsn.h
+	$(INSTALL) -D -m 0644 $(@D)/libtsn.so $(STAGING_DIR)/usr/lib/libtsn.so
+endef
+
+define TSNTOOL_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0644 $(@D)/libtsn.so $(TARGET_DIR)/usr/lib/libtsn.so
+	$(INSTALL) -D -m 0755 $(@D)/tsntool $(TARGET_DIR)/usr/bin/tsntool
+endef
+
+$(eval $(generic-package))
