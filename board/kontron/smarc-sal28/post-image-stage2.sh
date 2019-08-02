8a007179cd964777c6d3ce7fe1c95200a1a19e16
Heiko Thiery <heiko.thiery@kontron.com>
2019-08-02 15:15:11 +0200

    sl28: fix creation of zip file
    Junk Path inside the archive and rename the boot script file.
    
    Signed-off-by: Heiko Thiery <heiko.thiery@kontron.com>

--- board/kontron/smarc-sal28/post-image-stage2.sh
+++ board/kontron/smarc-sal28/post-image-stage2.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-${HOST_DIR}/bin/zip ${BINARIES_DIR}/spi-flash-updater.zip ${BINARIES_DIR}/spi-flash-updater.scr ${BINARIES_DIR}/spi-flash.img
+TMPDIR=$(mktemp -d)
+cp ${BINARIES_DIR}/spi-flash-updater.scr ${TMPDIR}/boot.scr
+cp ${BINARIES_DIR}/spi-flash.img ${TMPDIR}/
+${HOST_DIR}/bin/zip -j ${BINARIES_DIR}/spi-flash-updater.zip ${TMPDIR}/*
+rm -rf ${TMPDIR}
